5fb08f2b8207c33e786f30d705dee40f14d18a3c
OlhaLu <olhalutsyshyna@gmail.com>
2020-05-21 18:20:34 +0300

    add axios response

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,39 @@
 import axios from 'axios';
 
-const marvelApi = axios.create({
-  baseURL: 'https://gateway.marvel.com',
-  params: {
-    apikey: 'f367c5b99b3652be48ad9adf86fdb00b',
-  },
-});
+axios.defaults.baseURL = 'https://gateway.marvel.com/v1/public/';
+const apikey = 'f367c5b99b3652be48ad9adf86fdb00b';
 
-export default marvelApi;
+const getListComics = () => {
+  return axios
+    .get(`/comics?apikey${apikey}`)
+    .then(response => response.data)
+    .catch(error => error);
+};
+
+const getAllCharacters = () => {
+  return axios
+    .get(`/characters?apikey${apikey}`)
+    .then(response => response.data)
+    .catch(error => error);
+};
+
+const getCharacterById = characterId => {
+  return axios
+    .get(`characters/${characterId}?apikey=${apikey}`)
+    .then(response => response.data)
+    .catch(error => error);
+};
+
+const getListComicsCharacter = characterId => {
+  return axios
+    .get(`characters/${characterId}/comics?apikey=${apikey}`)
+    .then(response => response.data)
+    .catch(error => error);
+};
+
+export default {
+  getListComics,
+  getAllCharacters,
+  getCharacterById,
+  getListComicsCharacter,
+};
